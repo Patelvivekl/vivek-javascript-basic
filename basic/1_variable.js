7dc6e8d7644a3f50127128256f4b9ed7a207483b
Patelvivekl <168712023+Patelvivekl@users.noreply.github.com>
2024-05-02 08:05:18 +0000

    1_variable

---
+++ basic/1_variable.js
@@ -0,0 +1,8 @@
+const accountId =1001;
+let accountEmail = "vivek123@gmail.com";
+var account_password = "12221";
+
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,account_password]);
