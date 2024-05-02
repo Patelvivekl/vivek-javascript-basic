e5ac31007ea032badeeaf14e5f2d981997a18cae
Patelvivekl <168712023+Patelvivekl@users.noreply.github.com>
2024-05-02 08:04:46 +0000

    2_datatype

---
+++ basic/2_datatype.js
@@ -0,0 +1,17 @@
+let name = "vivek";
+let age = 21;
+let loggin = false;
+
+
+
+
+//number;
+//string;
+//boolean;
+//null;;
+//undefined;
+//symbol = unique;
+//object
+console.log(typeof "hitesh");
+console.log(typeof  1234);
+console.log(typeof null);
